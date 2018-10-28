c791accb17c86018ea41029c70af56d110090fb8
Richard Backhouse <richardabackhouse@gmail.com>
2018-10-28 11:18:49 -0400

    Add loading css

--- SearchScreen.js
+++ SearchScreen.js
@@ -353,5 +353,14 @@ const styles = StyleSheet.create({
 	backRightBtnRight: {
 		backgroundColor: 'darkgray',
 		right: 0
-	}
+	},
+    loading: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
