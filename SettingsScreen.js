7001cfbe8b4faad736b1ba67789f5035f8d97be1
Richard Backhouse <richardabackhouse@gmail.com>
2021-07-11 08:21:47 -0400

    Version Bump

--- SettingsScreen.js
+++ SettingsScreen.js
@@ -156,7 +156,7 @@ class AboutModal extends React.Component {
                     <View style={styles.flex3}>
                         <Text style={styles.text2}>About Maximum MPD</Text>
                     </View>
-                    <Text style={[styles.text1, {padding: 15}]}>Version: 4.8</Text>
+                    <Text style={[styles.text1, {padding: 15}]}>Version: 4.9</Text>
                     <Text style={[styles.text1, {padding: 15}]}>Author: Richard Backhouse</Text>
                     <Text style={[styles.text1, {padding: 15}]}>Various Images provided by Icons8 (https://icons8.com)</Text>
                     <View style={styles.flex1}>
